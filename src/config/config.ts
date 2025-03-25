a7ba3874112d0f3a41da0916e9744787a404739d
Javier0486 <javiermm1986@gmail.com>
2025-03-25 15:07:28 -0600

    Code refactor to make navigate method work with different URLs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
 export const BASE_URL = 'https://www.saucedemo.com';
+export const AEBASE_URL = 'https://www.automationexercise.com/';
 export const USERNAME = 'standard_user';
 export const PASSWORD = 'secret_sauce';
